fba9b39832c636189cc8a0e39e9c4aa678d22de7
tusqasi <2002.tushar.kuntawar@gmail.com>
2021-03-20 19:15:09 +0530

    Inital commit

---
+++ get_question.py
@@ -0,0 +1,31 @@
+from bs4 import BeautifulSoup
+import requests
+import sys
+
+# URL = sys.argv[1]
+URL = "https://codeforces.com/contest/570/problem/C"
+
+inputs = []
+outputs = []
+parsed_items = []
+
+page = requests.get(URL)
+soup = BeautifulSoup(page.content, 'html.parser')
+
+def get_io():
+   for _ in soup.find_all("br" ):
+      _.replace_with("\n")
+   
+   for _ in soup.find_all("pre"):
+       parsed_items.append("".join(_.contents))
+   
+   for _ in enumerate(parsed_items, start=1):
+       if _[0] % 2 == 0:
+           outputs.append(_[1])
+       else:
+           inputs.append(_[1])
+   return [inputs, outputs]
+def get_meta():
+    question_name = soup.find("div", class_="title")
+    # TODO: Make this robust
+    return question_name.contents[0].split('.',1)[1][1:].lower()
---
+++ put_tcs.py
@@ -0,0 +1,25 @@
+import os 
+import get_question
+
+os.chdir("../questions/") # Should this be hard coded or user defined
+existing_ques = os.getcwd()
+ques_name = get_question.get_meta()
+if (ques_name) in existing_ques:
+    if ques_name[-1].isdigit():
+        ques_num = int(ques_name.split("_")[-1])
+        ques_name = ques_name[:-1] + str(1+ques_num)
+    else:
+        ques_name += "_01"
+
+os.mkdir(ques_name)
+os.chdir(ques_name)
+# Beyond this point question_name is unique in question's directory
+# save get_question.inputs() to in.txt
+
+def write_this(file_name: str,data: list):
+    with open("file_name", mode='w') as fp:
+        for _ in data:
+            fp.write(_)
+
+write_this("in.txt", get_question.get_io()[0])
+write_this("out.txt", get_question.get_io()[1])
